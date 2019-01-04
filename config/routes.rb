632342f2e697be170c1c65d76714ea466145836c
tatsuya12 <tatsuya.0530.167@gmial.com>
2019-01-04 18:42:05 +0900

    create user-conrimation-route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 root 'items#index'
+get 'items/user_confirmation' => 'items#user_confirmation'
 resources :items
-get 'tweet/show' => 'tweet#show'
+# get 'tweet/show' => 'tweet#show'
 end

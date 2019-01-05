aa327c623068d43017c73a6f0bcf25a7e84fd4b4
eri <jta.e.m6@gmail.com>
2019-01-05 19:32:07 +0900

    マージプル

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  devise_for :users
 root 'items#index'
 get 'items/mypage' => 'items#mypage'
 get 'items/user_confirmation' => 'items#user_confirmation'
-resources :items
-=======
+
   devise_for :users, :controllers => {
    :registrations => 'users/registrations',
    :sessions => 'users/sessions',
@@ -19,12 +16,9 @@ resources :items
    get "sign_in", :to => "users/sessions#new"
    get "sign_out", :to => "users/sessions#destroy"
  end
-
-
-  root 'items#index'
-  get 'items/user_confirmation' => 'items#user_confirmation'
->>>>>>> master
   get 'items/detail' => 'items#detail'
   get 'items/show' => 'items#show'
+resources :items
+
 end
 

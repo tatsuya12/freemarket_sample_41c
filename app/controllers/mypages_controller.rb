4baa3d6be000203e69a713228c5840771877be76
tatsuya12 <tatsuya.0530.167@gmial.com>
2019-01-09 19:12:41 +0900

    edit maypages_controller

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -1,2 +1,17 @@
 class MypagesController < ApplicationController
+
+  def credit
+  end
+
+  def credit_detail
+  end
+
+  def user_confirmation
+  end
+
+  def profile
+  end
+
+  def logout
+  end
 end

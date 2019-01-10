21ca661a605efcbea439b33039ea4b148edaff51
eri <jta.e.m6@gmail.com>
2019-01-10 20:27:31 +0900

    下準備
    #What
    ・seedファイル記述
    ・link_toに変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
+
   def index
+  	@items = Item.includes(:user)
   end
 
   def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: "テスト１", email: "test@gmail.com", password: "password")
+Item.create(name: "アイテム１", price: "15000", status: "", size: "M", condition: "新品、未使用", introduction: "テスト商品紹介")
+Image.create(image: "https://static-mercari-jp-imgtr2.akamaized.net/thumb/photos/m68416746432_1.jpg?1545554718", item_id:"1")

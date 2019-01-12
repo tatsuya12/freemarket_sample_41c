df29583eb23452fc9d07163bb661c6ccf3db5674
akihiro190e <nishikawa_ssd@yahoo.co.jp>
2019-01-12 11:59:11 +0900

    modified item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
-  belongs_to :user
-  belongs_to :user, foreign_key: 'id', primary_key: 'saller_id_id', class_name: 'User'
-  belongs_to :user, foreign_key: 'id', primary_key: 'buyer_id_id', class_name: 'User'
+  belongs_to :seller, class_name: "User", foreign_key: "seller_id"
+  belongs_to :buyer, class_name: "User", foreign_key: "buyer_id"
+  has_many :images
   has_one :delivery
 end

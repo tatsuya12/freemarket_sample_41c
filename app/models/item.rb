2ca62d242fd530640fe0a9f20b315c754fd03728
akihiro190e <nishikawa_ssd@yahoo.co.jp>
2019-01-14 10:27:11 +0900

    delete association

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,8 +2,6 @@ class Item < ApplicationRecord
   belongs_to :seller, class_name: "User", foreign_key: "seller_id"
   belongs_to :buyer, class_name: "User", foreign_key: "buyer_id"
   has_many :images, dependent: :destroy
-  has_many :likes, dependent: :destroy
-  has_many :comments, dependent: :destroy
   has_one :delivery, dependent: :destroy
   belongs_to :brand
   belongs_to :category

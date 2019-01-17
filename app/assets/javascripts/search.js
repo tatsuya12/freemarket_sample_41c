cf5c9a7c83a59f73de8ceff6fcc2e8de7d59da2b
tatsuya12 <tatsuya.0530.167@gmial.com>
2019-01-17 19:26:35 +0900

    create search.js

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,4 +1,41 @@
 $(function() {
+
+  var search_list = $(".items-box-parent");
+  var search_result = $(".search-container__l-content__items__head")
+
+  function appendItem(item) {
+    var haml = `.items-box-child
+                  = link_to "" do
+                    .child-image
+                      - item.images.first(1).each do |image|
+                        =image_tag(image.image, class: "first-image")
+                    .child-content
+                      %h3.child-content__name
+                        = item.name
+                      .child-content__bottom
+                        .child-content__bottom__price
+                          = "¥#{item.price}"
+                        .child-content__bottom__likes
+                          %i.fa.fa-heart
+                            %span
+                              8
+                      .child-content__tax
+                        (税込)`
+    search_list.append(haml);
+  }
+
+  function appendNoItem(item) {
+    var message = `一致する商品はありません`
+    var result = `検索結果 0件`
+    search_list.append(message);
+    search_result.empty();
+    search_result.append(result);
+  }
+
+
+
+
+
   $(".input-search").on("keyup", function() {
     var input = $(".input-search").val();
 
@@ -10,14 +47,14 @@ $(function() {
     })
 
     .done(function(items) {
-      $(".listview.js-lazy-load-images").empty();
+      $(".items-box-parent").empty();
       if (items.length !== 0) {
-       items.forEach(function(product){
+       items.forEach(function(item){
          appendItem(item);
        });
       }
       else {
-       appendNoProduct("一致する商品はありません");
+       appendNoItem("一致する商品はありません");
       }
     })
   });

325aabbdc92aed08725bb2ce8361a5c7ed7ea6dd
eri <jta.e.m6@gmail.com>
2019-01-17 20:12:25 +0900

    Conflict cancellation

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,7 @@ class ItemsController < ApplicationController
   end
 
   def show
+  	render :layout => 'no-pankuzu'
   end
 
 end
--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -2,53 +2,31 @@ crumb :root do
   link "メルカリ", root_path
 end
 
-#マイページのルーティングを整えたらパス変更してね！
 crumb :mypage do
-  link "マイページ", root_path
+  link "マイページ", mypage_path
 end
 
-#プロフィールのルーティングを整えたらパス変更してね！
 crumb :mydata do |mydata|
-  link "プロフィール", root_path
+  link "プロフィール", profile_mypage_path
   parent :mypage
 end
 
-#本人情報の登録のルーティングを整えたらパス変更してね！
 crumb :user_confirmation do |user_confirmation|
-  link "本人情報の登録", root_path
+  link "本人情報の登録", user_confirmation_mypage_path
   parent :mypage
 end
 
-#ログアウトのルーティングを整えたらパス変更してね！
 crumb :logout do |logout|
-  link "ログアウト", root_path
+  link "ログアウト", sign_out_path
   parent :mypage
 end
 
-#支払い方法のルーティングを整えたらパス変更してね！
 crumb :credit do |credit|
-  link "支払い方法", root_path
+  link "支払い方法", credit_mypage_path
   parent :mypage
 end
 
-#クレジットカード情報入力のルーティングを整えたらパス変更してね！
 crumb :credit_detail do |credit_detail|
-  link "クレジットカード情報入力", root_path
+  link "クレジットカード情報入力", credit_detail_mypage_path
   parent :credit
 end
-
-# crumb :project_issues do |project|
-#   link "Issues", project_issues_path(project)
-#   parent :project, project
-# end
-
-# crumb :issue do |issue|
-#   link issue.title, issue_path(issue)
-#   parent :project_issues, issue.project
-# end
-
-# If you want to split your breadcrumbs configuration over multiple files, you
-# can create a folder named `config/breadcrumbs` and put your configuration
-# files there. All *.rb files (e.g. `frontend.rb` or `products.rb`) in that
-# folder are loaded and reloaded automatically when you change them, just like
-# this file (`config/breadcrumbs.rb`).

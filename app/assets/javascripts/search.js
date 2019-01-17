d08e5e2a40f78daa34ee0ba6a65ca6825d7d989c
tatsuya12 <tatsuya.0530.167@gmial.com>
2019-01-17 17:33:19 +0900

    add ajax to search.js

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,6 +1,24 @@
 $(function() {
   $(".input-search").on("keyup", function() {
     var input = $(".input-search").val();
-    console.log(input);
+
+    $.ajax({
+      type: 'GET',
+      url: '/items/search',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+
+    .done(function(items) {
+      $(".listview.js-lazy-load-images").empty();
+      if (items.length !== 0) {
+       items.forEach(function(product){
+         appendItem(item);
+       });
+      }
+      else {
+       appendNoProduct("一致する商品はありません");
+      }
+    })
   });
 });

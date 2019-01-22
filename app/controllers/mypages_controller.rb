67d77962fad532b3a12b0acdabc6dbfe896ba64e
eri <jta.e.m6@gmail.com>
2019-01-22 20:22:59 +0900

    Fix breadcrumbs
    #What
    -Swap application file and layout file
    
    #Why
    -Because render can not be used twice

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -1,27 +1,32 @@
 class MypagesController < ApplicationController
 
   def credit
+    render :layout => 'add-header&pankuzu'
   end
 
   def credit_detail
+    render :layout => 'add-header&pankuzu'
   end
 
   def user_confirmation
+    render :layout => 'add-header&pankuzu'
   end
 
   def profile
+    render :layout => 'add-header&pankuzu'
   end
 
   def logout
+    render :layout => 'add-header&pankuzu'
   end
 
   def sign_up_top
-    render :layout => 'no-header&pankuzu'
   end
 
   def show
     user = User.find(params[:id])
     @nickname = current_user.name
+    render :layout => 'add-header&pankuzu'
   end
 
 end

ab781b0d7c178ee8e47e5d9bb42b66e3358c54c5
akihiro190e <nishikawa_ssd@yahoo.co.jp>
2019-01-22 15:05:48 +0900

    create category.js

---
+++ app/assets/javascripts/category.js
@@ -0,0 +1,16 @@
+$(function(){
+// カテゴリーセレクト
+  $('#item_category_id').change(function(event, template) {
+    var val = $('#item_category_id').val();
+    var template = $(`#childrenCategoryOf${val}`);
+    $(this).nextAll().remove();
+    $(this).parent().append(template.html());
+    });
+
+  $(document).on('change', '.midium_categories', function(event, template) {
+    var val = $('.midium_categories').val();
+    var template = $(`#childrenCategoryOf${val}`);
+    $(this).nextAll().remove();
+    $(this).parent().append(template.html());
+  });
+})
